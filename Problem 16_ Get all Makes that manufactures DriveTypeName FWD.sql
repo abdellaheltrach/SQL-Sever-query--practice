514179ee5ebdcb91d05e7367bc26b9d52246a9c2
abdellaheltrach <abdellah.eltrach@gmail.com>
2024-08-13 13:50:41 +0200

    solved the 16th Problem about Getting all Makes that manufacture DriveTypeName = FWD

---
+++ Problem 16_ Get all Makes that manufactures DriveTypeName FWD.sql
@@ -0,0 +1,5 @@
+SELECT distinct Makes.Make, DriveTypes.DriveTypeName
+FROM     Makes INNER JOIN
+                  VehicleDetails ON Makes.MakeID = VehicleDetails.MakeID INNER JOIN
+                  DriveTypes ON VehicleDetails.DriveTypeID = DriveTypes.DriveTypeID
+where DriveTypes.DriveTypeName = 'FWD'
