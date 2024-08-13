b82d78afaa0118d18e9b3b228620e54a4b263a23
abdellaheltrach <abdellah.eltrach@gmail.com>
2024-08-13 13:28:42 +0200

    solved the 10th Problem about Getting all Makes that runs with GAS

---
+++ Problem 10_Get all Makes that runs with GAS.sql
@@ -0,0 +1,7 @@
+SELECT distinct Makes.Make, FuelTypes.FuelTypeName
+FROM     FuelTypes INNER JOIN
+                  VehicleDetails ON FuelTypes.FuelTypeID = VehicleDetails.FuelTypeID INNER JOIN
+                  Makes ON VehicleDetails.MakeID = Makes.MakeID
+				  where FuelTypes.FuelTypeName = N'GAS'
+				--  group by Makes.Make,FuelTypes.FuelTypeName
+				  order by Makes.Make
