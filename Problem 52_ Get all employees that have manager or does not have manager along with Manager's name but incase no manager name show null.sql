1c98cebc5f3e23151730cd203915ad7bfd287540
abdellaheltrach <abdellah.eltrach@gmail.com>
2024-08-19 16:32:29 +0200

    solved the 52th Problem about Getting all employees that have manager or does not have manager along with Manager's name but incase no manager name show null

---
+++ Problem 52_ Get all employees that have manager or does not have manager along with Manager's name but incase no manager name show null.sql
@@ -0,0 +1,5 @@
+SELECT Employees.Name, Employees.ManagerID, Employees.Salary, managers.Name AS ManagerName
+FROM     Employees LEFT OUTER JOIN
+                  Employees AS managers ON Employees.ManagerID = managers.EmployeeID
+
+
